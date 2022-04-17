e273382ec97772768d8cb98c705bf707fb8322d2
zman <manzhiyuan@lixiang.com>
2022-04-17 11:18:47 +0800

    修改文件命名方式

--- src/main.py
+++ src/main.py
@@ -5,9 +5,11 @@ from load_json import local_json
 from compute_location import compute_location
 from config import today_dir
 import os
+import time
 
 if __name__ == '__main__':
-    filepath = today_dir() + '1.csv'
+    timestr = time.strftime('%H:%M:%S', time.localtime(time.time()))
+    filepath = today_dir() + timestr + '.json'
     image = load_image_from_clipboard()
     recognition(image, filepath)
     df = local_json(filepath)
